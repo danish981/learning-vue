a5a0de54269a44b6af977d8a321dfbfb00d78f37
danish <danishmehmood981@gmail.com>
2024-08-06 18:57:00 +0500

    Changes related to emit rolled back, they were causing an issue idk

--- src/components/CoffeePlan.vue
+++ src/components/CoffeePlan.vue
@@ -1,11 +1,16 @@
 <script setup>
+import { ref } from "vue";
+
+const selected = ref(false);
+
 const props = defineProps({
   planName: { type: String, default: "The Default Plan", required: true },
 });
 </script>
 
 <template>
-  <div class="plan">
+  <!-- CHILD COMPONENT -->
+  <div class="plan" :class="{ 'active-plan': selected }">
     <div class="description">
       <span class="title">{{ planName }}</span>
     </div>
--- src/components/PlanPicker.vue
+++ src/components/PlanPicker.vue
@@ -1,11 +1,12 @@
 <script setup>
 import { ref } from "vue";
-// import CoffeePlan from "./CoffeePlan.vue";
+import CoffeePlan from "./CoffeePlan.vue";
 const plans = ref(["The Single", "The Curious", "The Addict", "The Hacker"]);
 </script>
 
-
 <template>
+  <!-- PARENT COMPONENT -->
+
   <div class="plans">
     <CoffeePlan v-for="plan in plans" :key="plan" :planName="plan" />
   </div>
